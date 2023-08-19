b50c5c0db9c62a03366929f4d96d6dc6ca2ffab5
chrisb <chrism.budnik@gmail.com>
2023-08-19 14:36:48 +0200

    create: init.py (youtube as module)

---
+++ youtube/__init__.py
@@ -0,0 +1,7 @@
+from .content import YoutubeContent
+from .video import Video
+from .playlist import Playlist
+from .channel import Channel
+from .search import YouTubeSearch
+from .trending import YoutubeTrending
+from .video_data_collector import VideoDataCollector
